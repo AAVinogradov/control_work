4e7a39ecc43d5cac5b3cc329c0ccc0e4118cc0fa
AAVinogradov <aavinogradov@bk.ru>
2023-07-27 20:51:47 +0300

    подправил данные

--- My_control_work/Program.cs
+++ My_control_work/Program.cs
@@ -8,7 +8,8 @@
 // 4. Написать программу, решающую поставленную задачу
 // 5. Использовать контроль версий в работе над этим небольшим проектом (не должно быть так, что всё залито одним коммитом, как минимум этапы 2, 3, и 4 должны быть расположены в разных коммитах)
 
-// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 // Примеры:
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
@@ -23,13 +24,13 @@ string[] GetMatrix()
     string[] array = new string[elementsCount];
     for(int i = 0; i < array.Length; i++)
     {
-        Console.Write($"Введите элемент масива под индексом {i}: ");
+        Console.Write($"Введите элемент масива под индексом {i}, например, какой-нибудь город и его регион на En раскладке: ");
         array[i] = Convert.ToString(Console.ReadLine())!;
     }
     return array; 
 }
 
-string[] ChangeData(string[] matrix1)
+string[] RemoveVowelLetters(string[] matrix1)
 {
     string[] matrix2 = new string[matrix1.Length];
     for (int i = 0; i < matrix1.Length; i++)
@@ -63,10 +64,10 @@ string[] ChangeData(string[] matrix1)
 }
 
 string[] myArray1 = GetMatrix();
-Console.WriteLine("Сгенерированный массив:");
-Console.WriteLine($"[{string.Join(", ", myArray1)}]");
+Console.WriteLine("Заданный массив:");
+Console.Write($"[{string.Join(", ", myArray1)}]");
 Console.WriteLine();
-string[] myArray2 = ChangeData(myArray1); 
-Console.WriteLine("Измененный массив:");
-Console.WriteLine($"[{string.Join(", ", myArray2)}]");
-
+string[] myArray2 = RemoveVowelLetters(myArray1); 
+Console.WriteLine("Полученный массив:");
+Console.Write($"[{string.Join(", ", myArray2)}]");
+Console.WriteLine();
